4ba4106b1b237f0fcad92342e87108b30c8f754a
florangel <florangelvalverde@upeu.edu.pe>
2020-05-24 17:27:46 -0500

    sistema_ventas

---
+++ js/script.js
@@ -0,0 +1,5 @@
+function cambio() {
+    document.getElementById("caja1").style.background= "red";
+    document.getElementById("caja2").style.color= "white";
+    document.getElementById("caja4").style.display= "none";
+}
